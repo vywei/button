12353127954c3e03eddfa4930625b87d427bf72c
Cory <cbaxes@gmail.com>
2018-01-19 15:56:36 -0800

    Initial Shop and Item outlines for issues 40 and 41 respectively.

--- src/Shop.java
+++ src/Shop.java
@@ -1,4 +1,21 @@
+import java.util.*;
 
 public class Shop {
-
+  
+  public ArrayList<Item> getShopItems(Player p) {
+    //TODO: get list of items available for purchase from Database
+  }
+  
+  public ArrayList<Item> getOwnedItems(Player p) {
+    //TODO: get list of items owned by p from Database
+  }
+  
+  public ArrayList<Item> getAllItems() {
+    //TODO: get all possible shop items from Database
+  }
+  
+  public int purchaseItem(Item i, Player p) {
+    //TODO: update player inventory and point balance with Database
+  }
+  
 }
