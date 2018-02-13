40dd8096b2b718c809248fb78977a6eaadf2233e
Victor Wei <vywei@Victors-MacBook.local>
2018-02-12 16:12:57 -0800

    Item(abstract) -> Skin, VisualEffect, SoundEffect structure

---
+++ src/logic/SoundEffect.java
@@ -0,0 +1,9 @@
+package logic;
+
+public class SoundEffect extends Item {
+  public SoundEffect(int id, String name, int price) {
+    this.id = id;
+    this.name = name;
+    this.price = price;    
+  }
+}
---
+++ src/logic/VisualEffect.java
@@ -0,0 +1,9 @@
+package logic;
+
+public class VisualEffect extends Item {
+  public VisualEffect(int id, String name, int price) {
+    this.id = id;
+    this.name = name;
+    this.price = price;    
+  }
+}
