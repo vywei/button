a7baab07174ff5edc8aaca565b327b03138da301
Tanna Kaul <tannajkaul@gmail.com>
2018-02-13 20:12:15 -0800

    small change

--- src/logic/BugReport.java
+++ src/logic/BugReport.java
@@ -4,18 +4,24 @@ public class BugReport{
 	private User reportUser;
 	private String reportMessage;
     /**
-     * BugReport constructor, 
+     * BugReport constructor 
      * @param user, bug report message
      */
 	private BugReport(User user, String message){
 		reportMessage = message;
 		reportUser = user;
 	}
-	
+    /**
+     * getter for the report message
+     * 
+     */
 	private String getReportMessage() {
 		return reportMessage;
 	}
-	
+    /**
+     * getter for the report user
+     * 
+     */
 	private User getUser(){
 		return reportUser;
 	}
