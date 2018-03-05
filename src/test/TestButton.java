e1770aa9bd2551b504f66a2f8149d7149096f3f0
cbaxes <pilotman1354@gmail.com>
2018-03-05 11:29:20 -0800

    Fixing build #66

--- src/test/TestButton.java
+++ src/test/TestButton.java
@@ -1,7 +1,7 @@
 package test;
 
 import static org.junit.Assert.*;
-import logic.Button;
+import logic.Btn;
 import logic.Skin;
 
 import org.junit.Test;
@@ -11,7 +11,7 @@ public class TestButton {
 	@Test
 	public void testButtonSetSkin() {
 		Skin s = new Skin(0, "Test Skin", 500);
-		Button b = new Button(s, 1);
+		Btn b = new Btn(s, 1);
 		b.setSkin(s);
 		assertEquals(b.getCurrentSkin(), s);
 	}
