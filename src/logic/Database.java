c0b61513dc750da032b8b2b583ea06eec35b5f77
Cory <cbaxes@gmail.com>
2018-03-05 15:33:42 -0800

    Work on issue #77

--- src/logic/Database.java
+++ src/logic/Database.java
@@ -2,10 +2,13 @@ package logic;
 
 import java.util.ArrayList;
 import java.util.List;
+import javax.xml.bind.DatatypeConverter;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import armdb.ConnectHost;             //import to make connection
 import armdb.QueryResult;
 import armdb.SQLQueryException;
@@ -54,6 +57,45 @@ public class Database {
     return ch;
   }
   
+  public User loginUser(String username, String password) {
+    User resultUser = null;
+    
+    MessageDigest md = null;
+    try {
+      md = MessageDigest.getInstance("MD5");
+    } catch (NoSuchAlgorithmException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+    md.update(password.getBytes());
+    byte[] digest = md.digest();
+    String passHash = DatatypeConverter.printHexBinary(digest).toUpperCase();
+
+    // Create query and result
+    SQLSelect query = new SQLSelect(ch);
+    QueryResult qr;
+
+    // Execute the query
+    try {
+        qr = query.result("player", new ArrayList<String>(), 
+            "WHERE username = '" + username + 
+            "' AND MD5(password) = '" + "'");
+
+        // Convert each row into an item
+        resultUser = parseUser(qr);
+    }
+    catch(SQLQueryException e){
+        System.out.println(e.getMessage());
+    }
+    
+    if (resultUser != null) {
+      return resultUser;
+    }
+    else {
+      return null;
+    }
+  }
+  
   /**
    * Updates user score in the database
    * @param u
@@ -91,9 +133,31 @@ public class Database {
       list.add(newUser);
     }
   }
+  
+  private User parseUser(QueryResult qr) {
+    // Loop through all rows in the result
+    User newUser = null;
+    
+    while(qr.nextFlag()) {
+      
+      // Parse the column data
+      int id = Integer.parseInt(qr.getValue("id"));
+      String username = (qr.getValue("username"));
+      int score = Integer.parseInt(qr.getValue("score"));
+      
+      //Instantiate new item and insert into result list
+      newUser = new User(username, id, score);
+    }
+    if (newUser != null) {
+      return newUser;
+    }
+    else {
+      return null;
+    }
+  }
     
     
-    public List<User> getLeaderboard() {
+  public List<User> getLeaderboard() {
 	// Create result list
 	List<User> players = new ArrayList<>();
 
