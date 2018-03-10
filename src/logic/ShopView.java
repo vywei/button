3f4b121541a75292a65c7342223b4ff51572c297
Mitchell <mover333@gmail.com>
2018-03-10 15:03:01 -0800

    properly wired up storeView issue #65

---
+++ src/logic/ShopView.java
@@ -0,0 +1,196 @@
+package logic;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.BorderStroke;
+import javafx.scene.layout.BorderStrokeStyle;
+import javafx.scene.layout.BorderWidths;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+
+public class ShopView 
+{
+    private static String t = "theme.css"; 
+    private final VBox view;
+    protected static Scene store;
+    boolean purchaseView = true;
+     
+    public ShopView()
+    { 
+      
+        User temp = Main.getUser();
+      
+        Sidebar sidebar = new Sidebar(temp);
+        temp.register((Observer) sidebar);
+        
+        VBox root = new VBox();
+        root.getChildren().addAll(getHeader(), getBody());
+        
+        BorderPane pane = new BorderPane();
+        pane.setRight(sidebar);
+        pane.setLeft(root);
+        pane.getStylesheets().add(getClass().getResource(t).toExternalForm());
+        pane.getStyleClass().add("root");
+        
+        view = root;
+    }
+    
+   public Node getView()
+   {
+       view.getStylesheets().add(getClass().getResource(t).toExternalForm());
+       view.getStyleClass().add("root");
+       return view;
+   }
+   
+   private HBox getHeader() {
+     Label title = new Label("Store");
+     title.getStylesheets().add(getClass().getResource(t).toExternalForm());
+     title.getStyleClass().add("a-header"); 
+     
+     HBox header = new HBox();
+     header.getChildren().add(title);
+     header.setAlignment(Pos.CENTER);
+     return header;
+     
+   }
+   
+   private HBox getBody() {
+     HBox body = new HBox();
+     
+     body.setAlignment(Pos.CENTER);
+     body.setPrefWidth(Main.SCREEN_WIDTH);
+     body.setPadding(new Insets(10,0,0,0));
+     
+     body.getChildren().add(getSwitcher());
+     /*if (purchaseView) {
+       body.getChildren().add(getPurchase());
+     } else {
+       body.getChildren().add(getCustomize());
+     }*/
+     return body;
+   }
+   
+   private HBox getSwitcher() {
+     Button purchase  = new Button("Purchase");
+     Button customize = new Button("Customize");
+     purchase.setMinWidth(200);
+     customize.setMinWidth(200);
+     
+     customize.setOnAction(new EventHandler<ActionEvent>() {
+       public void handle(ActionEvent event) {
+         purchaseView = false;
+       }
+     });
+     
+     purchase.setOnAction(new EventHandler<ActionEvent>() {
+       public void handle(ActionEvent event) {
+         purchaseView = true;
+       }
+     });
+     
+     HBox switcher = new HBox();
+     switcher.getChildren().addAll(purchase, customize);
+     switcher.setAlignment(Pos.CENTER);
+     
+     return switcher;
+   }
+   
+   private HBox getPurchase() {
+     List<Item> items = dummyList(); //Main.theShop.getAllItems();
+     HBox purchaseView = new HBox();
+     
+     for (Iterator<Item> i = items.iterator(); i.hasNext();) {
+       purchaseView.getChildren().add(genPurchaseFrame(i.next()));
+     }
+     return purchaseView;
+     
+   }
+   
+   private HBox getCustomize() {
+     List<Item> items = dummyList(); //Main.theShop.getOwnedItems(Main.user);
+     HBox customizedView = new HBox();
+     
+     for (Iterator<Item> i = items.iterator(); i.hasNext();) {
+       customizedView.getChildren().add(genCustomizeFrame(i.next()));
+     }
+     return customizedView;
+     
+   }
+   
+   private VBox genPurchaseFrame(Item item) {
+     VBox frame = new VBox();
+     frame.setMaxWidth(250);
+     frame.setMinWidth(250);
+     frame.setMaxHeight(250);
+     frame.setMinHeight(250);
+     
+     frame.setBorder(new Border(new BorderStroke(Color.BLACK, 
+         BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
+     
+     Label name = new Label(item.getName());
+     HBox title = new HBox();
+     title.getChildren().add(name);
+     title.setAlignment(Pos.CENTER);
+     
+     frame.getChildren().add(title);
+     
+     HBox priceNBuy = new HBox();
+     
+     Label price = new Label(Integer.toString(item.getPrice()));
+     Button buy = new Button("Buy");
+     
+     priceNBuy.getChildren().addAll(price, buy);
+     frame.getChildren().add(priceNBuy);
+     return frame;
+     
+   }
+   
+   private VBox genCustomizeFrame(Item item) {
+     VBox frame = new VBox();
+     frame.setMaxWidth(250);
+     frame.setMinWidth(250);
+     frame.setMaxHeight(250);
+     frame.setMinHeight(250);
+     
+     frame.setBorder(new Border(new BorderStroke(Color.BLACK, 
+         BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
+     
+     Label name = new Label(item.getName());
+     HBox title = new HBox();
+     title.getChildren().add(name);
+     title.setAlignment(Pos.CENTER);
+     
+     frame.getChildren().add(title);
+     
+     HBox equipBox = new HBox();
+     
+     Button equip = new Button("Use");
+     
+     equipBox.getChildren().add(equip);
+     frame.getChildren().add(equipBox);
+     return frame;
+     
+   }
+   
+     private List<Item> dummyList() {
+     Skin item1 = new Skin(123, "Cool Skin", 999);
+     Skin item2 = new Skin(456, "Cool Skin 2", 123);
+     ArrayList<Item> temp = new ArrayList<Item>();
+     temp.add(item1);
+     temp.add(item2);
+     return temp;
+   }
+}
