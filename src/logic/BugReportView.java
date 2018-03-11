3b160eee63ba39870f071a255f57ae439415adf2
Tanna Kaul <tannajkaul@gmail.com>
2018-03-11 14:49:55 -0700

    code smells

--- src/logic/BugReportView.java
+++ src/logic/BugReportView.java
@@ -26,18 +26,18 @@ public class BugReportView {
     }
     public BugReportView()
     {
-    	Label header = new Label("Report A Bug");
-        header.setMaxWidth(Double.MAX_VALUE);
-        header.setAlignment(Pos.CENTER);
-        header.getStylesheets().add(getClass().getResource(t).toExternalForm());
-        header.getStyleClass().add("a-header"); 
+    	Label h = new Label("Report A Bug");
+        h.setMaxWidth(Double.MAX_VALUE);
+        h.setAlignment(Pos.CENTER);
+        h.getStylesheets().add(getClass().getResource(t).toExternalForm());
+        h.getStyleClass().add("a-header"); 
     	
-        GridPane homeGrid = new GridPane();
-        homeGrid.setPadding(new Insets(0, 0, 0, 0));
-        homeGrid.setVgap(8);
-        homeGrid.setHgap(10);
-        homeGrid.getStylesheets().add(getClass().getResource(t).toExternalForm());
-        homeGrid.getStyleClass().add("root");
+        GridPane hGrid1 = new GridPane();
+        hGrid1.setPadding(new Insets(0, 0, 0, 0));
+        hGrid1.setVgap(8);
+        hGrid1.setHgap(10);
+        hGrid1.getStylesheets().add(getClass().getResource(t).toExternalForm());
+        hGrid1.getStyleClass().add("root");
 
         User temp = Main.getUser();
         
@@ -72,7 +72,7 @@ public class BugReportView {
         vbButtons.setPadding(new Insets(30, 30, 30, 30));
         vbButtons.setMinWidth(450);
         vbButtons.setAlignment(Pos.CENTER);
-        vbButtons.getChildren().addAll(header, userInput,bugRep,submitButton);
+        vbButtons.getChildren().addAll(h, userInput,bugRep,submitButton);
         
         settingsBox.getChildren().addAll(vbButtons);
 
@@ -82,7 +82,7 @@ public class BugReportView {
         root.getStylesheets().add(getClass().getResource(t).toExternalForm());
         root.getStyleClass().add("root");
 
-        homeGrid.getChildren().addAll(root);
+        hGrid1.getChildren().addAll(root);
        
         view = root;
     }
