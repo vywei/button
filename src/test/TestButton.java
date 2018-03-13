03e220903215632307cc210af690605879df425e
Mitchell <mover333@gmail.com>
2018-03-13 00:45:54 -0700

    fixed test merge

--- src/test/TestButton.java
+++ src/test/TestButton.java
@@ -10,13 +10,8 @@ public class TestButton {
 
 	@Test
 	public void testButtonSetSkin() {
-<<<<<<< HEAD
-		Skin s = new Skin(0, "Test Skin", 500, "", "");
+	    Skin s = new Skin(0, "Test Skin", 500, "", "", "");
 		Btn b = new Btn(s, 1, null);
-=======
-		Skin s = new Skin(0, "Test Skin", 500, "", "", "");
-		Btn b = new Btn(s, 1);
->>>>>>> ca6527a383b07df092cbd83e038a8f57944489ed
 		b.setSkin(s);
 		assertEquals(b.getCurrentSkin(), s);
 	}
