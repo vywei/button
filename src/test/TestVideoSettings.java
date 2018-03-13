61f8a370d34c666d830eb0ada9f7fc16137f304c
Tanna Kaul <tannajkaul@gmail.com>
2018-03-13 11:45:38 -0700

    More unit tests

--- src/test/TestVideoSettings.java
+++ src/test/TestVideoSettings.java
@@ -14,7 +14,7 @@ public class TestVideoSettings {
 		Settings s = new Settings(testUser);
 		s.setTextureQual(5);
 		int result = s.getTextureQual();
-		assertEquals(5,result);
+		assertEquals(0,result);
 	}
 	
 	@Test
@@ -23,7 +23,46 @@ public class TestVideoSettings {
 		Settings s = new Settings(testUser);
 		s.setEffectsQual(5);
 		int result = s.getEffectsQual();
+		assertEquals(0,result);
+	}
+	
+	@Test
+	public void testAudioEnabled() {
+		User testUser = new User("testuser", "testpassword");
+		Settings s = new Settings(testUser);
+		s.setAudioEnabled(5);
+		int result = s.getAudioEnabled();
+		assertEquals(0,result);
+	}
+	
+	@Test
+	public void testMusicVol() {
+		User testUser = new User("testuser", "testpassword");
+		Settings s = new Settings(testUser);
+		s.setMusicVol(5);
+		int result = s.getMusicVol();
 		assertEquals(5,result);
 	}
+	
+	@Test
+	public void testEffectsVol() {
+		User testUser = new User("testuser", "testpassword");
+		Settings s = new Settings(testUser);
+		s.setEffectsVol(5);
+		int result = s.getEffectsVol();
+		assertEquals(5,result);
+	}
+	
+	@Test
+	public void testMusicPath() {
+		User testUser = new User("testuser", "testpassword");
+		Settings s = new Settings(testUser);
+		s.setMusicPath("/home/OopsIDidItAgain.mp3");
+		String result = s.getMusicPath();
+		assertEquals("/home/OopsIDidItAgain.mp3",result);
+	}
+	
+	
+
 
 }
