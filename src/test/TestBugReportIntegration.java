9e32607e0b8f5e23335df7b24c3b35d61acef213
Mitchell <mover333@gmail.com>
2018-03-13 22:33:25 -0700

    add Mitchell integration test

---
+++ src/test/TestBugReportIntegration.java
@@ -0,0 +1,27 @@
+package test;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import logic.BugReport;
+import logic.User;
+
+
+public class TestBugReportIntegration {
+
+    @Test
+    public void testBugReportIntegration() {
+        boolean result = false;
+        User testUser = new User("testuser", "testpassword");
+        BugReport b = new BugReport(testUser, "test message");
+        
+        User newUser = b.getUser();
+        String message = b.getReportMessage();
+        if (newUser.equals(testUser)  &&
+                message.equals("test message")) {
+            result = true;
+        }
+        assertEquals(true,result);
+    }
+
+
+}
