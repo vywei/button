4cc22872b4ecdfa77c5e802a8a41811e9c140025
Tanna Kaul <tannajkaul@gmail.com>
2018-03-14 10:52:51 -0700

    resolve code smells 'Swap these 2 arguments so they are in the correct order: expected value, actual value.'

--- src/test/TestSoundEffect.java
+++ src/test/TestSoundEffect.java
@@ -9,12 +9,12 @@ public class TestSoundEffect {
   @Test
   public void TestGetPrice() {
     SoundEffect e = new SoundEffect(123, "testEffect", 467);
-    assertEquals(e.getPrice(), 467);
+    assertEquals(467,e.getPrice());
   }
   
   @Test
   public void TestGetID() {
     SoundEffect e = new SoundEffect(123, "testEffect", 467);
-    assertEquals(e.getID(), 123);
+    assertEquals(123,e.getID());
   }
 }
