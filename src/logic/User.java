98e983fc04eef325987f3ea0caf2805e8f21a0bf
Mitchell <mover333@gmail.com>
2018-03-14 17:27:00 -0700

    removed System.out.print

--- src/logic/User.java
+++ src/logic/User.java
@@ -89,7 +89,6 @@ public class User implements Subject, Observer {
      * @param newSkin
      */
     public void changeSkin(Skin newSkin) {
-      System.out.println("ID: " + id);
 	Skin tempSkin = validateSkin(newSkin);
 		if (tempSkin != null) {
 		    currentSkin = tempSkin;
